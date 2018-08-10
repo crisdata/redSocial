ff34377189ea0b7d863839bc5175fa00cb21e71a
CristianG <tapcrt@gmail.com>
2018-08-09 19:51:42 -0500

    CG - Autenticacion con facebook (configurando inicio y validacion

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
+
+  def self.from_omniauth(auth)
+    where(provider: auth[:provider], uid:auth[:uid]).first_or_create do |user|
+      if auth[:info]
+        user.email = auth[:info] [:email]
+        user.name = auth[:info] [:name]
+      end
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
+
+
+# Tipos de metodos en programacion orientada a objetos:
+# Clase => User.from_omniauth()
+# Instancia => user = User.new ; user.from_omniauth()
