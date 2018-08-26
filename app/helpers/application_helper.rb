74325d2288e5b95f9b8c2eb7423983453459ad16
CristianG <tapcrt@gmail.com>
2018-08-26 18:33:01 -0500

    CG-Edicion y configuracion del registro de usuarios con helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def resource
+    @resource ||= User.new # asigna lo que tenemos al lado derecho ||=, a la variable del lado izquiendo, cuando esa llegue a ser nula o no exista
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource_class
+    User
+  end
 end
