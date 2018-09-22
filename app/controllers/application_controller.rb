8c452fdf596de2c9b3fbc5aacb9840f52f8f8e3e
CristianG <tapcrt@gmail.com>
2018-09-21 22:50:50 -0500

    CG-multiples layouts para las vistas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  layout :set_layout
+
+  protected
+  def set_layout
+    'application'
+  end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,4 +2,11 @@ class MainController < ApplicationController
   def home; end
 
   def unregistered; end
+
+  # Este es el hijo que esta sobrescribiendo en el padre
+  protected
+  def set_layout
+    return 'landing' if action_name == 'unregistered'
+    super
+  end
 end
